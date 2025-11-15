7e323efb80c80c14e6a0dcb8b3a91d2dc5f413c5
Star Developer 4870 <devstar4870@gcplab.me>
2025-11-15 16:55:54 +0000

    yes please generate one logo. the most sleek, minimal and modern looking

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,9 +12,15 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 2L2 7l10 5 10-5-10-5z" />
-      <path d="M2 17l10 5 10-5" />
-      <path d="M2 12l10 5 10-5" />
+      <path d="M8 20V14c0-2.2 1.8-4 4-4s4 1.8 4 4v6" />
+      <path d="M7 20h10" />
+      <path d="M12 4v2" />
+      <path d="M4 12H2" />
+      <path d="M20 12h2" />
+      <path d="m19.07 19.07-.42-.42" />
+      <path d="m5.35 5.35-.42-.42" />
+      <path d="m19.07 5.35-.42.42" />
+      <path d="m5.35 19.07-.42.42" />
     </svg>
   );
 }
